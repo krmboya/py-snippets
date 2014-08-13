f200368eba95d86a5b4a0437516ac30b2d0ad6b0
Kenny Rachuonyo <krmboya@gmail.com>
2014-08-13 11:25:37 +0300

    added list flattening

--- snippets.py
+++ snippets.py
@@ -59,7 +59,55 @@ while data.set(foo()):
 """
 
 # Get the intersect of two dicts
-print [k for k in some_dict if k in another_dict]
+
+# >>> print [k for k in some_dict if k in another_dict]
+
 # or
-print filter(some_dict.has_key, another_dict.keys())
+
+# >>> print filter(some_dict.has_key, another_dict.keys())
+
+
+# flattening a deeply-nested list
+# -----------------
+
+# define a function `is_scalar` that returns True or False based on whether
+# and item is scalar or not
+
+def flatten(sequence, item_is_scalar, result=None):
+    if result is None: result = []
+    for item in sequence:
+        if item_is_scalar(item):
+            result.append(item)
+        else:
+            flatten(item, item_is_scalar, result)
+    return result
+
+# if memory is an issue, generators can be used instead
+
+# a definition of `is_scalar`. 
+# check if item is iterable using built-in `iter`
+
+def is_scalar(item):
+    try: iter(item)
+    except: return True
+    else: return False
+
+# however a string will cause cause: RuntimeError: maximum recursion depth exceeded
+# Why? 
+
+# upgrade is_scalar to treat strings as scalar items
+
+def is_string(item):
+    try:
+        item + ''
+    except TypeError:
+        return False
+    else:
+        return True
+
+def is_scalar_upgraded(item):
+    return is_string(item) or is_scalar(item)
+
+# see related solutions: http://stackoverflow.com/questions/952914/making-a-flat-list-out-of-list-of-lists-in-python
+
 
